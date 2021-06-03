80aed575550cca064c3bf573fe953023786b52f7
Felipe Printes <felipe.unilasalle@gmail.com>
2021-06-03 09:46:59 -0300

    Add json_construct function with responsible to construct a json with user data

--- app/model/user.py
+++ app/model/user.py
@@ -1,7 +1,18 @@
+"""Main function responsible to set user information"""
 def main():
     nome  = "fulano"
     idade = 25
 
+    json_construct(nome, idade)
+
+"""Function responsible to build a json with user data"""
+def json_construct(nome, idade):
+    json_user_information = {
+        "nome": nome,
+        "idade": idade
+    }
+
+    return json_user_information 
 
 
 if __name__=="__main__":
